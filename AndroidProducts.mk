c8a4e5f9789959c7b5c052703cb0e6eb71985b79
heilbron <88156370+linux-sudo@users.noreply.github.com>
2023-10-20 22:58:33 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gta4l.mk
+    $(LOCAL_DIR)/evolution_gta4l.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gta4l-user \
-    lineage_gta4l-userdebug \
-    lineage_gta4l-eng
+    evolution_gta4l-user \
+    evolution_gta4l-userdebug \
+    evolution_gta4l-eng
