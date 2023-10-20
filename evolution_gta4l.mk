d22f41d9498ce3fa3d9a5a2da8942cf3b8bf2633
heilbron <88156370+linux-sudo@users.noreply.github.com>
2023-10-20 23:08:56 +0300

    Update evolution_gta4l.mk

--- evolution_gta4l.mk
+++ evolution_gta4l.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/gta4l/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
+$(call inherit-product, vendor/evolution/config/common_full_tablet.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gta4l
